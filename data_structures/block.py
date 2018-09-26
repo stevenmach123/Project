343904d117c4dcdb896c5cfd7cd34bcce4379705
karol <kstola2@uic.edu>
2018-09-25 19:44:13 -0500

    Added stubs

---
+++ data_structures/block.py
@@ -0,0 +1,19 @@
+# imports
+
+# hash function
+
+# create block
+
+# is valid
+
+# blockchain class
+
+# add block
+
+# top
+
+# height
+
+# genesis
+
+# proof of work
---
+++ data_structures/block.test.py
@@ -0,0 +1,19 @@
+# imports
+
+# unit test class
+
+# test hash function
+
+# test create block
+
+# test is valid
+
+# test add block
+
+# test top
+
+# test height
+
+# test genesis
+
+# test proof of work
