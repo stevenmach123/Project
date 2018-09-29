d8fc3ed23b86b207ae954ee911b94d6a3c9bdc4e
kstola2 <kstola2@uic.edu>
2018-09-29 11:37:12 -0500

    Added data structure lesson

--- data_structures/block.py
+++ data_structures/block.py
@@ -1,19 +1,106 @@
 # imports
+from hashlib import sha256 as sha
+from binascii import hexlify, unhexlify
+from time import time
 
 # hash function
+# takes in a string
+# returns a SHA-256 encoded hex-string
+
+
+def hashSHA(string):
+    return hexlify(sha(string.encode()).digest()).decode()
 
 # create block
+# takes in hash of previous block
+# returns a dictionary object with:
+# hash of previous block, data field, and the newly created block's hash
+
+
+def createBlock(data, prevHash):
+    blockHash = hashSHA(prevHash + data)
+    return {
+        'prevHash': prevHash,
+        'data': data,
+        'blockHash': blockHash
+    }
 
 # is valid
+# takes in two blocks
+# checks to see if the second block's previous hash field
+# is equivalent to the first block's hash field
+
+
+def isValid(blockA, blockB):
+    return blockB['prevHash'] == blockA['blockHash']
 
 # blockchain class
+# contains the actual list of blocks and
+# corresponding operations
 
-# add block
 
-# top
+class Blockchain:
 
-# height
+    def __init__(self):
+        self.chain = []
+
+    # add block
+    # takes in a block
+    # adds it to the end of the chain
+    def addBlock(self, block):
+        self.chain.append(block)
+
+    # top
+    # returns the last block in the chain
+    def top(self):
+        return self.chain[-1]
+
+    # height
+    # returns the height (length) of the chain
+    def height(self):
+        return len(self.chain)
 
 # genesis
+# creates a block, but uses a null hash as the previous hash
+
+
+def genesis():
+    prevHash = "0"*64
+    data = "genesis"
+    return createBlock(data, prevHash)
+
+# to integer
+# takes in a byte string as an argument
+# returns an integer with big endian byte order
+
+
+def toInt(bytestring):
+    return int.from_bytes(unhexlify(bytestring), byteorder='big')
 
 # proof of work
+# takes in data, a previous hash, and a target
+# works to calculate a hash integer value less than the target
+# does this by "incremental guessing"
+# timestamp and nonce update each time we "swing the pick axe"
+# once it's found, the output is like a traditional block
+# but with the new fields as well
+
+
+def createBlockPoW(data, prevHash, target):
+    nonce = 0
+    timestamp = int(time())
+    blockHash = hashSHA(data + prevHash + str(timestamp) +
+                        str(target) + str(nonce))
+    while not toInt(blockHash) < target:
+        nonce += 1
+        timestamp = int(time())
+        blockHash = hashSHA(
+            data + prevHash + str(timestamp) + str(target) + str(nonce))
+    return {
+        'prevHash': prevHash,
+        'data': data,
+        'timestamp': timestamp,
+        'target': target,
+        'nonce': nonce,
+        'blockHash': blockHash
+    }
--- data_structures/block.test.py
+++ data_structures/block.test.py
@@ -1,19 +1,93 @@
 # imports
+import unittest
+from block import * 
+import time
 
 # unit test class
+class TestBlock(unittest.TestCase):
 
-# test hash function
+    # test hash function
 
-# test create block
+    def testHashSHA(self):
+        # testHashSHA part 1:
+        # take two different strings
+        # check to see if hashes are different
+        a = "apple"
+        b = "orange"
+        hashA = hashSHA(a)        
+        hashB = hashSHA(b)
+        self.assertNotEqual(hashA, hashB)
 
-# test is valid
+        # testHashSHA part 2:
+        # take two long strings with a single letter changed
+        # check to see if hashes are different
+        s1 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        s2 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minin veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        hashS1 = hashSHA(s1)
+        hashS2 = hashSHA(s2)
+        self.assertNotEqual(hashS1, hashS2)
 
-# test add block
+        # testHashSHA part 3:
+        # take two of the same strings
+        # hash them separately
+        # check to see if the hashes are equivalent
+        c = "melon"
+        d = "melon"
+        hashC = hashSHA(c)
+        hashD = hashSHA(d)
+        self.assertEqual(hashC, hashD)
 
-# test top
+    # test is valid
+    # check to see if a block points backwards to a previous block
+    def testIsValid(self):
+        blockA = createBlock('this is block a', '000')
+        blockB = createBlock('this is block b', blockA['blockHash'])
+        self.assertTrue(isValid(blockA, blockB))
 
-# test height
+    # test add block
+    # check to see if when adding a block it is contained in the chain
+    def testAddBlock(self):
+        bc = Blockchain()
+        testBlock = createBlock('this is a test', '123456789')
+        bc.addBlock(testBlock)
+        self.assertEqual(bc.chain[0], testBlock)
 
-# test genesis
 
-# test proof of work
+    # test top
+    # check to see if calling top returns the last block in the chain
+    def testTop(self):
+        bc = Blockchain()
+        testBlock = createBlock('this is a test', '123456789')
+        bc.addBlock(testBlock)
+        self.assertEqual(bc.top(), testBlock)
+
+    # test height
+    # check to see if height returns the length of the chain
+    def testHeight(self):
+        bc = Blockchain()
+        testBlock = createBlock('this is a test', '123456789')
+        bc.addBlock(testBlock)
+        bc.addBlock(testBlock)
+        bc.addBlock(testBlock)
+        bc.addBlock(testBlock)
+        self.assertEqual(4, bc.height())
+
+    # test proof of work
+    # check to see if a proof of work mined block
+    # contains a hash that's less than the target
+    def testPoW(self):
+        data = 'testPoW'
+        prevHash = '000000'
+        target = 10**70       # play around with this exponent (stick to the 60-100 range)
+        print("Mining...")
+        a = int(time.time())
+        b = createBlockPoW(data, prevHash, target)
+        print("Block found!")
+        c = int(time.time())
+        print("Time it took: {} seconds".format((c-a)))
+        self.assertLessEqual(toInt(b['blockHash']), target)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
